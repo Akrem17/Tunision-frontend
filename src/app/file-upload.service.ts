a7e53c7b27ae6342fef975fa4e94222fcf5404b2
Akrem Hammami <akrem@gmx.us>
2021-09-06 14:44:39 +0100

    full project

---
+++ src/app/file-upload.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class FileUploadService {
+
+  constructor(private http:HttpClient) { }
+  baseApiUrl = "https://file.io"
+
+  upload(file):Observable<any> {
+  
+    // Create form data
+    const formData = new FormData(); 
+      
+    // Store form name as "file" with file data
+    formData.append("file", file, file.name);
+      
+    // Make http post request over api
+    // with formData as req
+    return this.http.post(this.baseApiUrl, formData)
+}
+}
